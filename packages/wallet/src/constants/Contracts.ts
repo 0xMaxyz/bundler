41610a8e07495ef64cb6d50ea4a05e4d7cc37963
sam <meisamtaher71@gmail.com>
2024-04-05 17:24:49 +0330

    change smart account factory

--- packages/wallet/src/constants/Contracts.ts
+++ packages/wallet/src/constants/Contracts.ts
@@ -1,6 +1,6 @@
 export const EntryPointAddress = '0x0000000071727De22E5E9d8BAf0edAc6f37da032'
 export const SmartAccountFactoryAddress =
-  '0x4729CCf7a9c43F83A3cdc7B666DBb53adc9cc851'
+  '0xa3733c15CE4Af3D9976016a2F2aa8f920c54c4ce'
 export const PaymasterAddress = '0xc523FF9698230096d4aDa45D52FA0063E109618D'
 export const bundlerUrl = 'https://bundler.aanansi.xyz'
 export const Domain = 'ans'
